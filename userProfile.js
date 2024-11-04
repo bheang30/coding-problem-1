3fb68253aa5db7267d587ae118fa5f61ad99fe03
bheang30 <solmorobhea20@gmail.com>
2024-11-04 18:20:36 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Adriane Bhea R. Solmoro";
+let age = 19;
+let favoriteNumber = 30;
+let favoriteColor = "black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
